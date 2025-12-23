7a14065f2eaad674e534db94f06e9068b34f66e5
jackzet743 <carlosmoya.pro2002@gmail.com>
2025-12-23 11:08:26 +0100

    Añadido el metodo POST para añadir un equipo a un club especifico con el ID y el metodo GET para obtener los equipos de un club, Hay un error cuando el club no existe, no lo indica

--- Backend/routes/teams.js
+++ Backend/routes/teams.js
@@ -4,17 +4,68 @@ const db = require('../config/db'); //  Importa la conexion con la base de datos
 
 
 router.get('/',(req, res)=>{ //Define una ruta.
-    const sql = 'SELECT * FROM teams';
+    const {id_club}=req.query;//Lee parametros de la query.
 
-    db.query(sql,(err, results)=> { //err por si falla y results para las filas de la db.
-        //Error en consola tipo 500.
-        if(err){
-            console.error(err);
-            return res.status(500).json({error:'Database error'});
-        }
-        //Devuelve todo en formato JSON.
-        res.json(results);
-    });
+    if (id_club) {
+        
+        const sql = 'SELECT * FROM teams WHERE id_club = ?';
+
+        db.query(sql, [id_club], (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Database error' });
+            }
+            res.json(results);
+        });
+
+    } else {
+        
+        const sql = 'SELECT * FROM teams';
+
+        db.query(sql, (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ error: 'Database error' });
+            }
+            res.json(results);
+        });
+    }
 });
+
+router.post('/', async(req, res)=>{
+    const{id_club, category}=req.body; //coge los datos del cliente
+
+    if(!id_club || !category){
+        return res.status(400).json({ //Control de que se pongan los datos
+            error: 'id_club and category required'
+        });
+    }
+
+    try{//Consulta
+        const [result]= await db.promise().query(` 
+            INSERT INTO teams (id_club, category)
+            VALUES (?, ?)`, [id_club, category]
+        );
+        //Respuesta por el servidor
+        res.status(201).json({
+            message: "Team created succesfully",
+            id_team: result.insertId
+        });
+
+    }catch{
+        console.error(error);
+
+        if(error.code === 'ER_NO_REFERENCES_ROW_2'){
+            return res.status(400).json({
+                error: 'Club does not exist'
+            });
+        }
+    
+        res.status(500).json({
+            error: 'Error creating team'
+        });
+    }
+
+})
 
 module.exports = router; //Se puede exportar a otros archivos.
