9811af4cf5d8a46b9d4550b9ffba44592902e750
jackzet743 <carlosmoya.pro2002@gmail.com>
2025-12-26 21:59:47 +0100

    Introduccion del metodo post de si el usuario es directivo puede meter personas en los equipos

--- Backend/routes/usersTeams.js
+++ Backend/routes/usersTeams.js
@@ -1,8 +1,10 @@
 const express = require('express'); //Importación de libreria.
 const router = express.Router(); //Es un miniservidor para las rutas relacionadas con teams.
 const db = require('../config/db'); //  Importa la conexion con la base de datos.
+const auth = require('../middleware/auth');
+const role = require('../middleware/role');
 
-router.post('/', async(req, res)=>{
+router.post('/', auth, role(['directivo']), async(req, res)=>{
     const {id_user, id_team, id_rol} = req.body;
 
     if(!id_user || !id_team || !id_rol){
@@ -11,75 +13,67 @@ router.post('/', async(req, res)=>{
         })
     }
 
-    const connection = await db.promise().getConnection();
 
     try{
         //Comprobación de usuario.
-        const [resultUser] = await connection.query(
-            'Select id_user, id_club from users where id_user = ?',
+        const [director] = await db.promise().query(
+            'Select id_club from users where id_user = ?',
+            [req.user.id_user] //Se pide desde la base de datos y no desde la api.
+        );
+
+        const id_club = director[0].id_club;
+
+        const [team] = await db.promise().query(`
+            SELECT id_team FROM teams WHERE id_team = ? AND id_club = ?`,
+            [id_team, id_club]
+        );
+
+        if(team.length === 0){
+            return res.status(404).json({error:'Team not found'});
+        }
+        
+        const [user] = await db.promise().query(
+            'Select id_user from users where id_user = ?',
             [id_user]
         );
 
-        if(resultUser.length === 0){
-            await connection.rollback();
+        if(user.length === 0){
             return res.status(404).json({error:'User not found'});
         }
-        //Comprobación de equipo
-        const [resultTeam] = await connection.query(
-            'Select id_team, id_club from teams where id_team = ?',
-            [id_team]
-        );
 
-        if(resultTeam.length===0){
-            await connection.rollback();
-            return res.status(404).json({error:'Team not found'});
-        }
-
-        const [resultRol] = await connection.query(
+        const [resultRol] = await db.promise().query(
             'Select id_rol from rol where id_rol = ?',
             [id_rol]
         );
 
         if(resultRol.length === 0){
-            await connection.rollback();
-            return res.status(404).json({error: 'Rol not found'});
-        }
-
-        //Tienen que ser del mismo club
-        if(resultTeam[0].id_club !== resultUser[0].id_club){
-            await connection.rollback();
-            return res.status(404).json({
-                error: 'User and team not from the same club'
-            });
+            return res.status(404).json({error: 'Role not found'});
         }
 
         //Sentar las relaciones
-        await connection.query(
+        await db.promise().query(
             `Insert into user_team (id_user, id_team, id_rol)
             values (?,?,?)`,
             [id_user,id_team,id_rol]
         );
 
-        await connection.commit();
         res.status(201).json({
-            message: 'User assigned to team succesfully'
+            message: 'User assigned to team successfully'
         });
 
-
     }catch (error){
-        await connection.rollback();
-        console.error(error);
+        
 
         if(error.code === 'ER_DUP_ENTRY'){
             return res.status(400).json({
                 error: 'User already assigned to this team'
             })
         }
+        console.error(error);
+
         res.status(500).json({
             error: 'Error assigning user to team'
         });
-    } finally{
-        connection.release();
     }
 
 });
