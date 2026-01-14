30051632dcbd4b8b662e1969b9043092997eac5f
jackzet743 <carlosmoya.pro2002@gmail.com>
2026-01-14 16:09:27 +0100

    Refactorización de app.js

--- Backend/app.js
+++ Backend/app.js
@@ -1,5 +1,12 @@
-const express = require('express'); //Importa la libreria express
-const db = require('./config/db'); //  Importa la conexion con la base de datos.
+// ==================================================
+// IMPORTACIONES
+// ==================================================
+
+const express = require('express'); // Importa la libreria express, Framework que genera el servidor.
+const db = require('./config/db'); //  Importa la conexion con la base de datos, MySQL.
+
+// RUTAS
+
 const clubsRoutes = require('./routes/clubs'); 
 const teamsRoutes = require('./routes/teams');
 const usersRoutes = require('./routes/users');
@@ -7,24 +14,38 @@ const usersRolRoutes = require('./routes/usersRol');
 const usersTeamsRoutes= require('./routes/usersTeams');
 const authRoutes = require('./routes/auth');
 
-const app = express(); //Convierte esto en mi servidor. Cuelga de la constante APP.
-const PORT = 3000;//Puerto donde se escucha el servidor.
 
-app.use(express.json());//Permite entender json al servidor.
-app.use('/clubs', clubsRoutes); //Permite usar el router de clubs.
-app.use('/teams', teamsRoutes); //Permite usar el router de teams.
-app.use('/users', usersRoutes);
-app.use('/user_Rol', usersRolRoutes);
-app.use('/user_team', usersTeamsRoutes);
-app.use('/auth', authRoutes);
+// ====================================================
+// CONFIGURACIÓN
+// ====================================================
 
-//Genera una respuesta a una petición y respuesta.
+const app = express(); // Convierte esto en mi servidor.
+const PORT = 3000; // Puerto donde se escucha el servidor.
+
+// ====================================================
+// MIDDLEWARE GLOBALES
+// ====================================================
+
+app.use(express.json()); // Permite entender json al servidor.
+
+// RUTAS
+
+app.use('/clubs', clubsRoutes); // /clubs/...
+app.use('/teams', teamsRoutes); // /teams/...
+app.use('/users', usersRoutes); // /users/...
+app.use('/user_Rol', usersRolRoutes); // /user_rol/...
+app.use('/user_team', usersTeamsRoutes); // /user_team/...
+app.use('/auth', authRoutes); // /auth/...
+
+// RUTA RAÍZ
 app.get('/', (req, res) => {
     res.send('Backend running');
 });
 
+// =================================================
+// ARRANQUE DEL SERVIDOR
+// =================================================
 
-//Arranca el servidor y empieza a escuchar peticiones por el puerto 3000
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 });
