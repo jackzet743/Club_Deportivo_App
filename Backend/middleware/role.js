f3935a80d101adb6d304eab1284ec73ba9525429
jackzet743 <carlosmoya.pro2002@gmail.com>
2026-01-15 11:18:32 +0100

    Refactorización del role.js

--- Backend/middleware/role.js
+++ Backend/middleware/role.js
@@ -1,23 +1,36 @@
-module.exports = function (allowedRoles = []) {
+// ============================================
+// MIDDLEWARE DE CONTROL DE ROLES
+// ============================================
+
+//Recibe un array de los roles permitidos.
+module.exports = function roleMiddleware (allowedRoles = []) {
+
     return (req, res, next) => {
 
-    // auth middleware debe haber puesto req.user
+    // Comprueba si auth ya puso al usuario, sino tiene lo bloquea
     if (!req.user || !req.user.roles) {
+
         return res.status(403).json({
         error: 'No roles found'
         });
+
     }
 
-    const hasRole = req.user.roles.some(role =>
+    //.some lo que hace es ver si al menos tiene 1 de los roles permitidos.
+    const hasPermission = req.user.roles.some(role =>
         allowedRoles.includes(role)
     );
 
-    if (!hasRole) {
+    //Si no tiene los permisos bloqueado.
+    if (!hasPermission) {
+
         return res.status(403).json({
         error: 'Forbidden: insufficient permissions'
         });
+
     }
 
     next(); // ✔ tiene permiso
+    
     };
 };
